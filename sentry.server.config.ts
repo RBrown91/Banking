595d64f26748646efd01e9bbf6f1c44c8589bb4f
Lunchbox91 <ryanallenbrown@gmail.com>
2024-06-01 16:33:50 +0100

    removed sentry replay

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -15,9 +15,4 @@ Sentry.init({
 
   // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
   // spotlight: process.env.NODE_ENV === 'development',
-
-  integrations: [Sentry.replayIntegration()],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0,
 });
